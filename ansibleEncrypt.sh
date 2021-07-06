5f5fb2c4826ef653e3bcf45e1e0e884fbfe65a05
Mik Kröis <mik.pedersen@ufst.dk>
2021-07-06 15:54:34 +0200

    secrets updated

--- ansibleEncrypt.sh
+++ ansibleEncrypt.sh
@@ -4,6 +4,7 @@ File="src/main/groovy/accountcheck/secrets/secrets.groovy"
 
 if grep -q AES256 "$File"; then
   eval "ansible-vault decrypt $File"
+  eval "git update-index --assume-unchanged src/main/groovy/accountcheck/secrets/secrets.groovy"
 else
   eval "ansible-vault encrypt $File"
 fi
